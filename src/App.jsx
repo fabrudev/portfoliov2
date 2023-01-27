43fca455575e03b89b6cd55747c07d84135c7a5d
FabruDJ <fabrizarateok@gmail.com>
2023-01-27 16:50:39 -0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './routes/Home'
 import Projects from './routes/Projects'
 
 function App() {
 
   return (
-    <HashRouter>
-    <Routes>
-      <Route path='/' element= {<Home/>} />
-      <Route path='/proyectos' element= {<Projects/>} />
-    </Routes>
-    </HashRouter>
+      <HashRouter>
+      <Routes>
+        <Route path='/' element= {<Home/>} />
+        <Route path='/proyectos' element= {<Projects/>} />
+      </Routes>
+      </HashRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavbarStyles.css'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <p>Fabru-<span>Dev</span></p>
+            <a href="mailto:fabrizarateok@gmail.com">Enviar email</a>
         </div>
     );
 };
